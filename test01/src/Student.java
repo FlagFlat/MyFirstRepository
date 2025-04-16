35a412562bf45c5c93f393add76c1be0a314df9d
FlagFlat <2601854329@qq.com>
2025-04-16 16:19:00 +0800

    第二次进行数据提交

--- test01/src/Student.java
+++ test01/src/Student.java
@@ -1,2 +1,7 @@
 public class Student {
+    public static void main(String[] args) {
+        System.out.println("nihao");
+        System.out.println("111");
+        System.out.println("dhwidh");
+    }
 }
